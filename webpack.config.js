b7af6bf162e10dc21bd1666b5770ea5f09bd6bf9
Lucas Pazelo <lucas_2991@hotmail.com>
2018-12-02 20:54:32 -0200

    Added devServer rule to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -61,14 +61,14 @@ module.exports = {
     })
   ],
 
-  // devServer: {
-  //   compress: true,
-  //   contentBase: path.resolve(__dirname, 'public/'),
-  //   publicPath: path.resolve(__dirname, 'public/'),
-  //   host: '0.0.0.0',
-  //   public: '0.0.0.0:8080',
-  //   port: '8080',
-  //   historyApiFallback: true
-  // }
+  devServer: {
+    compress: true,
+    contentBase: path.resolve(__dirname, 'public/'),
+    publicPath: path.resolve(__dirname, 'public/'),
+    host: '0.0.0.0',
+    public: '0.0.0.0:8080',
+    port: '8080',
+    historyApiFallback: true
+  }
 
 };
